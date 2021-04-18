e1575c1500e980a29474f25a5c82389db76e051b
luhuaei <luhuaei@gmail.com>
2021-04-18 21:00:48 +0800

    use timer implement delay

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
+51单片机 A2 开发板
+
+# 延迟时间
+
+  - [参考](https://www.engineersgarage.com/knowledge_share/internal-timer-registers-of-8051/)
 
 # LED
+
+开发板上面的LED灯是一端是连接电源的正极(VCC), 而想要LED发亮，则需要将LED的阴极的电压调低。
+
 ```
 sudo ./download.sh led
 ```
--- led/led.c
+++ led/led.c
@@ -1,8 +1,32 @@
 #include <8052.h>
 
-#define LED P2_0
+void delay(int i) {
+        while(i--) {
+                /* spend 2ms */
+                TMOD = 0x01;    /* time mode register */
 
-void main()
-{
-        LED=0;
+                TH0 = 0xF8;     /* timer high byte */
+                TL0 = 0xCC;     /* timer low byte */
+
+                TR0 = 1;        /* the 1th timer run */
+                while(!TF0);    /* the 1th timer overflow */
+                TR0 = 0;        /* the 1th timer stop */
+                TF0 = 0;        /*  */
+        }
+}
+
+void carousel() {
+        while(1) {
+                for (int i=0; i < 8; i++) {
+                        /*
+                           0 is light, 1 is the off, ~ make the all 0 bit to 1
+                        */
+                        P2=~(0x01<<i);
+                        delay(5000);
+                };
+        };
+}
+
+void main() {
+        carousel();
 }
